f6aa0475d1ca38a9ec0bfe5122318900667890a0
Rajat Mishra <63346695+Rajat1517@users.noreply.github.com>
2023-10-03 13:10:25 +0530

    Update index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,6 +2,32 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoDB = require("./db");
+const https = require('https');
+
+exports.handler = async (event, context) => {
+ const url = 'https://datavisualisation.onrender.com';
+
+ return new Promise((resolve, reject) => {
+   const req = https.get(url, (res) => {
+     if (res.statusCode === 200) {
+       resolve({
+         statusCode: 200,
+         body: 'Server pinged successfully',
+       });
+     } else {
+       reject(
+         new Error(`Server ping failed with status code: ${res.statusCode}`)
+       );
+     }
+   });
+
+   req.on('error', (error) => {
+     reject(error);
+   });
+
+   req.end();
+ });
+};
 
 mongoDB();
 
