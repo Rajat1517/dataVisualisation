52c75f477bb360d8dc97f5cd42026db4de9266c2
Rajat Mishra <rajatmishra0815@gmail.com>
2023-10-08 22:35:16 +0530

    Solving server sleep issue

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoDB = require("./db");
-const job = require ("./Routes/cron-job")
-mongoDB();
+const cron = require('cron');
+const https = require("https");
 
-job.start();
+
+mongoDB();
 
 app.get('/', (req, res) => {
   res.send('Server is Working!')
@@ -17,6 +18,24 @@ app.use("/api", require("./Routes/data"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+const backendUrl = "https://datavisualisation.onrender.com";
+
+const job = new cron.CronJob("*/14 * * * *", function(){
+    https.get(backendUrl, (res)=>{
+        if(res.statusCode === 200){
+            console.log("server restarted!");
+        }
+        else{
+            console.erro("failed to restart");
+        }
+    }).on('error', (err)=>{
+        console.error(err.message);
+    });
+});
+
+
+job.start();
 
 
 
